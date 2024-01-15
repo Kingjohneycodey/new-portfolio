c053ac047ca292d7cd231637128cc88cd0219b3b
Kingjohneycodey <kvngjohnny10@gmail.com>
2024-01-15 23:56:51 +0100

    new commit

---
+++ components/Main/AboutSection/AboutItems.tsx
@@ -0,0 +1,107 @@
+import { m } from "framer-motion"
+import Image from "next/image"
+import { aboutVariant } from "../../../variants/aboutVariant"
+
+const AboutItems = () => {
+  return (
+    <>
+      {" "}
+      <div className="flex flex-col items-center justify-center lg:flex-row lg:space-x-10 ">
+        {/* text */}
+        <div className="text lg:flex-[45%]">
+          <m.h1
+            className="flex items-start mb-6 space-x-2 text-xl font-normal text-white lg:text-2xl font-pop lg:space-x-3"
+            initial={{ opacity: 0, y: 20 }}
+            variants={aboutVariant}
+            whileInView={aboutVariant.heading}
+            viewport={{ once: true }}
+          >
+            <p>Hi{"👋, "} I&apos;m</p>
+            <p className="relative font-semibold font-pop text-cyan w-fit">
+              Ahiakwo John,
+            </p>
+          </m.h1>
+          {/* info */}
+          <div className="mb-10 md:mb-12 lg:mb-0">
+            <m.p
+              className="mb-6 text-lg font-normal text-text lg:text-xl"
+              variants={aboutVariant}
+              initial={{ opacity: 0, y: 25 }}
+              whileInView={aboutVariant.p1}
+              viewport={{ once: true }}
+            >
+              An intelligent software engineering student, As a results and performance
+              driven Fullstack Web Developer with one year of professional
+              experience, specializing in crafting interactive and visually
+              appealing user interfaces. Proficient in leveraging the latest
+              technologies and design systems, my goal is to deliver superior,
+              user-centric solutions that align with both client and company
+              requirements.
+            </m.p>
+            <m.p
+              className="mb-6 text-lg font-normal text-text lg:text-xl"
+              variants={aboutVariant}
+              initial={{ opacity: 0, y: 25 }}
+              whileInView={aboutVariant.p2}
+              viewport={{ once: true }}
+            >
+              My approach involves a deep understanding of project requirements,
+              collaborating closely with cross-functional teams to make informed
+              decisions that ensure optimal functionality. Committed to
+              delivering the best and most user-friendly experiences, I am adept
+              at meeting project goals efficiently and on schedule.
+            </m.p>
+            <m.p
+              className="mb-6 text-lg font-normal text-text lg:text-xl"
+              variants={aboutVariant}
+              initial={{ opacity: 0, y: 25 }}
+              whileInView={aboutVariant.p2}
+              viewport={{ once: true }}
+            >
+              Transformng my creativity and knowledge into websites have been my
+              passion for months now. During this time, I have helped various
+              clients establish their presence online. I also love learning new
+              technologies and frameworks.
+            </m.p>
+            <m.p
+              className="mb-6 text-lg font-normal text-text lg:text-xl md:mb-11"
+              initial={{ opacity: 0, y: 20 }}
+              variants={aboutVariant}
+              whileInView={aboutVariant.p3}
+              viewport={{ once: true }}
+            >
+              Additionally, i am a lover of open source softwares and i&apos;m
+              also looking for internships and projects to boost my skills and
+              to get experience in Frontend Development.
+            </m.p>
+          </div>
+        </div>
+        {/* images/illustrsions */}
+        <m.div
+          className="lg:flex-[40%] w-full md:max-w-md relative cursor-pointer group"
+          initial={{ opacity: 0, y: 20 }}
+          variants={aboutVariant}
+          whileInView={aboutVariant.image}
+          viewport={{ once: true }}
+        >
+          <div className="w-full md:w-full h-[28rem] md:h-[37rem] lg:h-[30rem] xl:h-[34rem] relative rounded-md overflow-hidden border-2 border-cyan backdrop-blur-sm bg-cyan/20 z-20 ">
+            <div className="relative w-full h-full duration-500 grayscale group-hover:grayscale-0">
+              <Image
+                src={"/images/me.jpg"}
+                alt="moi"
+                priority={true}
+                fill
+                quality={100}
+                placeholder={"blur"}
+                blurDataURL={"/images/me.jpg"}
+              />
+            </div>
+          </div>
+          <div className="absolute h-[28rem] md:w-[105%] md:h-[37.5rem] lg:h-[30rem] xl:h-[34.5rem] w-full top-5 left-3 border-2 border-body z-10 bg-buttonBg rounded-lg group-hover:top-4 group-hover:left-3 duration-500"></div>
+        </m.div>
+      </div>
+    </>
+  )
+}
+
+export default AboutItems
---
+++ components/Main/HeroSection/HeroSection.tsx
@@ -0,0 +1,111 @@
+import Text from "../../Text/Text"
+import { AboutIcon } from "../icons"
+import HeroNav from "./HeroNav"
+import HireMeButton from "./HireMeButton"
+import ResumeButton from "./ResumeButton"
+import { m } from "framer-motion"
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col w-full">
+      {/* text */}
+      <div className="relative w-full mx-auto">
+        {/* name */}
+        <div className="w-full font-bold text-cyan font-pop">
+          <m.p
+            className="mb-4 text-lg font-pop md:text-xl md:mb-6"
+            initial={{ y: 20, opacity: 0 }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 0.3, delay: 0.55 },
+            }}
+          >
+            Hi there, 👋I&apos;m
+          </m.p>
+          <m.h1
+            className="flex items-center space-x-5 pb-7 font-pop text-text text-3xl sm:text-4xl md:text-[5rem] md:leading-[1.3]"
+            initial={{ y: 20, opacity: 0 }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 0.3, delay: 0.6 },
+            }}
+          >
+            <span className=" w-fit">Ahiakwo</span>
+            <span className="relative text-cyan before:absolute before:-bottom-2 before:left-0 before:w-full before:h-1 md:before:h-2 md:before:-bottom-3 before:block before:bg-cyan before:rounded-full">
+              John.
+            </span>
+          </m.h1>
+        </div>
+        {/* brief about */}
+        <div className="w-full mb-6 ">
+          <m.p
+            className="text-text_Light text-3xl sm:text-4xl md:text-6xl font-semibold md:leading-[1.3]"
+            initial={{ y: 20, opacity: 0 }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 0.3, delay: 0.7 },
+            }}
+          >
+            I build things for the web.
+          </m.p>
+        </div>
+        {/* brief about info */}
+        <div className="max-w-[40.75rem]">
+          <m.p
+            className="text-text_Light text-base md:text-xl md:leading-[1.3] "
+            initial={{ y: 20, opacity: 0 }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 0.3, delay: 0.8 },
+            }}
+          >
+            As a<span className="text-cyan"> Full Stack Developer,</span> I
+            specialize in building and launching dynamic user interfaces and
+            experiences that captivate both clients and companies. I harness the
+            power of the <Text text="efficient technologies " /> and{" "}
+            <Text text="design systems " /> to create interactive solutions that
+            leave a lasting impression. My primary objective is to deliver the
+            best, easiest, and most functional long-term solutions that not only
+            meet the requirements of the company but also exceed the
+            expectations of clients. With a strong commitment to user
+            satisfaction, I strive to create seamless and engaging experiences
+            that seamlessly blend aesthetic appeal with practical functionality.
+          </m.p>
+        </div>
+        {/* buttons */}
+        <m.div
+          className="flex items-center w-full mb-10 space-x-5 md:space-x-7 mt-11 md:mt-14"
+          initial={{ y: 20, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: { duration: 0.3, delay: 0.9 },
+          }}
+        >
+          <HireMeButton />
+          <ResumeButton />
+        </m.div>
+        {/* btn2 */}
+        <m.div
+          className="flex items-center space-x-3"
+          initial={{ y: 20, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: { duration: 0.3, delay: 0.9 },
+          }}
+        >
+          <HeroNav name="learn about me">
+            <AboutIcon height="4" width="4" />
+          </HeroNav>
+        </m.div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
